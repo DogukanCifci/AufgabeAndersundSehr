645a771fcd1c0b63a7fc1605c7292acab6c73587
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2023-02-28 23:24:13 +0100

    footer completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,10 @@ import "../styles/footer.scss";
 //React Icons Import
 import { IoLocationOutline } from "react-icons/io5";
 import { AiOutlineMail } from "react-icons/ai";
+import { HiOutlineDevicePhoneMobile } from "react-icons/hi2";
+import { FaFax, FaFacebookF, FaTwitter, FaGooglePlusG } from "react-icons/fa";
+import { RiPinterestFill } from "react-icons/ri";
+import { MdOutlineKeyboardArrowUp } from "react-icons/md";
 
 const Footer = () => {
   return (
@@ -19,20 +23,46 @@ const Footer = () => {
           <div className="footer-icon">
             <AiOutlineMail />
           </div>
-          <div className="adress">New York 1584, St. Vincent</div>
+          <div className="adress">
+            <a href="mailto:contact@architenecxt.com">
+              contact@architenecxt.com
+            </a>
+          </div>
         </div>
         <div className="each-icon">
           <div className="footer-icon">
-            <IoLocationOutline />
+            <HiOutlineDevicePhoneMobile />
           </div>
-          <div className="adress">New York 1584, St. Vincent</div>
+          <div className="adress">
+            <a href="tel:+18004506024">+1 800 450 6024</a>
+          </div>
         </div>
         <div className="each-icon">
           <div className="footer-icon">
-            <IoLocationOutline />
+            <FaFax />
           </div>
-          <div className="adress">New York 1584, St. Vincent</div>
+          <div className="adress">
+            <a href="tel:+18004506024">+1 800 745 6024</a>
+          </div>
         </div>
+      </div>
+
+      <div className="logos">
+        <div className="each-logos">
+          <FaFacebookF />
+        </div>
+        <div className="each-logos">
+          <FaTwitter />
+        </div>
+        <div className="each-logos">
+          <RiPinterestFill />
+        </div>
+        <div className="each-logos">
+          <FaGooglePlusG />
+        </div>
+      </div>
+      <div className="up-button">
+        <MdOutlineKeyboardArrowUp />
       </div>
     </div>
   );
