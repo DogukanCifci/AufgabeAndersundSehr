a234890f0b56695fed12b9ff042e41adbdd41889
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2023-02-28 23:00:19 +0100

    footer edited

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,41 @@
 import React from "react";
 //Style Import
 import "../styles/footer.scss";
+//React Icons Import
+import { IoLocationOutline } from "react-icons/io5";
+import { AiOutlineMail } from "react-icons/ai";
 
 const Footer = () => {
-  return <div className="footer-container">Footer</div>;
+  return (
+    <div className="footer-container">
+      <div className="footer-icon-container container grid grid-cols-2 md:grid-cols-4">
+        <div className="each-icon">
+          <div className="footer-icon">
+            <IoLocationOutline />
+          </div>
+          <div className="adress">New York 1584, St. Vincent</div>
+        </div>
+        <div className="each-icon">
+          <div className="footer-icon">
+            <AiOutlineMail />
+          </div>
+          <div className="adress">New York 1584, St. Vincent</div>
+        </div>
+        <div className="each-icon">
+          <div className="footer-icon">
+            <IoLocationOutline />
+          </div>
+          <div className="adress">New York 1584, St. Vincent</div>
+        </div>
+        <div className="each-icon">
+          <div className="footer-icon">
+            <IoLocationOutline />
+          </div>
+          <div className="adress">New York 1584, St. Vincent</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
