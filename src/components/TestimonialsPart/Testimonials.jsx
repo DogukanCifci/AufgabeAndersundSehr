eadefe988720e5eeaf8f138a3d83d9ebff5e8b9c
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2023-03-01 14:30:50 +0100

    slider added

--- src/components/TestimonialsPart/Testimonials.jsx
+++ src/components/TestimonialsPart/Testimonials.jsx
@@ -2,52 +2,51 @@ import React from "react";
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContextProvider";
 import { MyButton } from "../../styles/ButtonStyle";
+
 //Style Import
 import "../../styles/testimonials.scss";
 
 //Swiper
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { useSwiper } from "swiper/react";
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
+//Data import
+import { data } from "./data";
+import TestimonialsCards from "./TestimonialsCards";
 
 const Testimonials = () => {
-  const { setOpen } = useContext(AuthContext);
-
+  const swiper = useSwiper();
   return (
-    <div className="testimonials-container" onClick={() => setOpen(false)}>
-      <div className="content ">
-        <div className="left-part">
-          <h1 className="title">What people say</h1>
-          <div className="tirnak-isareti">
-            <img
-              src="https://imgrosetta.mynet.com.tr/file/14683261/14683261-728xauto.jpg"
-              alt=""
-            />
-            <div className="description">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. At
-              delectus rem sapiente officiis excepturi itaque quia dolorem
-              commodi maiores quibusdam hic, ut a atque ex dicta suscipit quod
-              modi necessitatibus!
-            </div>
-          </div>
-          <div className="author">-Kale, SkyNet</div>
-          <MyButton className="testimonials-button" type="button">
-            Read More
-          </MyButton>
-        </div>
-        <div className="right-part-picture">
-          <img
-            src="https://assets.contenthub.wolterskluwer.com/api/public/content/ce98168dc425442682983e286b4c2096"
-            alt=""
-          />
-        </div>
-      </div>
-    </div>
+    <Swiper
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log("slide change")}
+      onClick={() => swiper.slideNext()}
+    >
+      {data.map((element, idx) => {
+        return (
+          <SwiperSlide key={idx}>
+            <TestimonialsCards element={element} />
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
   );
 };
 
